5af0d52e9e567c1ea10a27edc98a662191edb4da
Joko Nugroho <jocoding@gmail.com>
2023-10-08 21:51:51 +0700

    nomor absen

--- src/absen.js
+++ src/absen.js
@@ -24,7 +24,7 @@ function tambahDataAbsen(nomor) {
 
   const cariSiswa = dataSiswa.find(d => d.nomor === nomor)
   if (typeof cariSiswa === 'undefined') {
-    alert('Data Siswa tidak ditemukan!')
+    alert(`Data Siswa dengan Nomor Absen ${nomor} tidak ditemukan! `)
     return
   }
   const now = new Date();

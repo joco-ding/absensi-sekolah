7b224f936649648b6ac9b66b3d042ff8801c80cb
Joko Nugroho <jocoding@gmail.com>
2023-10-08 21:17:50 +0700

    scan button

--- src/qrscanner.js
+++ src/qrscanner.js
@@ -1,16 +1,22 @@
 bMulaiScan.addEventListener('click', startQRScan)
 
 function startQRScan() {
+  videoElement.setAttribute('height', '300')
   codeReader.decodeFromVideoDevice(null, videoElement, (result, err) => {
     if (result) {
       // Hasil pemindaian berhasil
       console.log(result.text);
       hasilElement.innerText = result.text;
-      codeReader.reset();
+      stopQRScan()
     }
 
     if (err && !(err instanceof ZXing.NotFoundException)) {
       console.error(err);
     }
   });
+}
+
+function stopQRScan() {
+  codeReader.reset();
+  videoElement.setAttribute('height', '0')
 }
